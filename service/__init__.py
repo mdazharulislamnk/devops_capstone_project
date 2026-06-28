86bd21b7207870b92e618df7dbd9d8d2161d7536
mdazharulislamnk <mdazharulislamnk@gmail.com>
2026-06-28 21:38:29 +0600

    Add Talisman security headers

--- service/__init__.py
+++ service/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_talisman import Talisman
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///accounts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+talisman = Talisman(app)
 
 from service import routes, models
